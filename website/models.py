04230f21a8273e2eec8846e286ec31aeff327dd6
Yuliya <yuliyahvan@yahoo.com>
2019-09-24 20:07:40 +0600

    Update in models contact_number as blank=True instead of null=True; Update test login for status code==200 with follow=True;

--- website/models.py
+++ website/models.py
@@ -4,7 +4,7 @@ from django.contrib.auth.models import AbstractUser
 
 class CustomUser(AbstractUser):
     birthday = models.DateField(null=True, blank=True)
-    contact_number = models.CharField(max_length=20, null=True)
+    contact_number = models.CharField(max_length=20, blank=True)
 
 
 class Image(models.Model):
